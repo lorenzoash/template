89367dbd8eeb2d03f2a8bc40011affb804845e53
Lorenzo Ash <z.lakyo@gmail.com>
2020-01-30 13:23:05 -0800

    html template

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using emailTemplate.Models;
+
+namespace emailTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        [Route("/home")]
+        public IActionResult Index()
+        {
+            return View("Email");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
